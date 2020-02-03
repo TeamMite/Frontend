ee38316f5f34e735886e615b206cbb07a479488a
TeamMite <miteteam6@gmail.com>
2020-02-03 16:06:09 +0530

    academic year drop down is added

--- src/app/analytics/analytics.module.ts
+++ src/app/analytics/analytics.module.ts
@@ -4,13 +4,15 @@ import { CommonModule } from '@angular/common';
 import { AnalyticsRoutingModule } from './analytics-routing.module';
 import { AnalyticsComponent } from './analytics.component';
 import { Statement6Component } from './statement6/statement6.component';
+import { HttpClientModule } from '@angular/common/http';
 
 
 @NgModule({
   declarations: [AnalyticsComponent, Statement6Component],
   imports: [
     CommonModule,
-    AnalyticsRoutingModule
+    AnalyticsRoutingModule,
+    HttpClientModule
   ]
 })
 export class AnalyticsModule { }
--- src/app/analytics/statement6/statement6.component.ts
+++ src/app/analytics/statement6/statement6.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AnalyticsService } from '../analytics.service';
 
 @Component({
   selector: 'app-statement6',
@@ -6,10 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./statement6.component.css']
 })
 export class Statement6Component implements OnInit {
-
-  constructor() { }
+  academicYear:string[]=[];
+  constructor(private analyticsService:AnalyticsService ) {
+   }
 
   ngOnInit() {
+      this.analyticsService.get_academics_years().subscribe(res=>
+        {
+          this.academicYear=res["year"];
+        
+    })
+    }
   }
+  
 
-}
+
